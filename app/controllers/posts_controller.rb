8753d220fa3d18bd23c59b8274148a0aa17e7d01
Sumair <sumairqaisar992@gmail.com>
2022-05-13 19:08:07 +0500

    Add controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+    
+    def index
+    render 'post_list'
+    end
+
+    def show
+    render 'post_details'
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController 
+
+    def index
+        render 'user_list'
+    end
+
+    def show
+        render 'user_details'
+    end
+
+end
