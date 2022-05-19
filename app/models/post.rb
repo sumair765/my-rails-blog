3e11cdcb6cce93d468f75bea03d8461910b5d374
Sumair <sumairqaisar992@gmail.com>
2022-05-20 03:54:43 +0500

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-    has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-    has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
