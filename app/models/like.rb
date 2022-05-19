c59090b1b92fe86d6e445ff56225553d13255e5f
Sumair <sumairqaisar992@gmail.com>
2022-05-20 04:29:13 +0500

    Make requested changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author
+  belongs_to :post, foreign_key: 'post_id'
 
   def update_likes_counter
     post.increment!(:likes_counter)
