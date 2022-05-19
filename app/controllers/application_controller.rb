2d3ab500d1724ada7b79b3fcbe542b710f45b845
Sumair <sumairqaisar992@gmail.com>
2022-05-20 04:03:42 +0500

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-      # A current_user method that returns the first user from the database.
+  # A current_user method that returns the first user from the database.
   def current_user
     User.first
   end
