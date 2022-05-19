c3a49e6ba614cc7061d81d8a133b81d0f869f903
Sumair <sumairqaisar992@gmail.com>
2022-05-20 03:50:50 +0500

    Add Associations and custom method in Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+    has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+    has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 end
