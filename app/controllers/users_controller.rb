83df22e8717d91a9fa05a7f1185bcb2355f32663
Sumair <sumairqaisar992@gmail.com>
2022-05-21 16:41:45 +0500

    Add new Controllers for likes and comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @current_user = current_user
     render 'user_list'
   end
 
-  def shows
+  def show
+    @user = User.find(params[:id])
     render 'user_details'
   end
 end
