5ac0eb6f69ad5377220325751df9b8f419050370
Sumair <sumairqaisar992@gmail.com>
2022-05-22 00:46:15 +0500

    Modify Controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:posts).find(params[:id])
     render 'user_details'
   end
 end
