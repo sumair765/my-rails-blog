938d44918915cbb789de677373d9d55a21f837fb
Sumair <sumairqaisar992@gmail.com>
2022-05-28 22:25:57 +0500

    Update User's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :comments, foreign_key: :user_id
   has_many :posts, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
+
+  after_create :generate_api_token
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
@@ -17,5 +19,12 @@ class User < ActiveRecord::Base
 
   def admin?
     :role == 'admin'
+  end
+
+  private
+
+  def generate_api_token
+    self.api_token = Devise.friendly_token
+    save
   end
 end
